1025ae8f885d4910e9d41ffd7c42f6092991ee15
Hannu Raappana <hannu.t.raappana@gmail.com>
2025-09-01 12:56:25 +0300

    Update client to use TLS

--- src/main-client.c
+++ src/main-client.c
@@ -1,5 +1,7 @@
 
 #include "logging.h"
+#include "tls-connection.h"
+#include "session.h"
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -7,13 +9,14 @@
 #include <unistd.h>
 #include <errno.h>
 
-static const char* ip = "192.168.1.137";
+static const char* ip = "127.0.0.1";
 static const uint16_t port = 6666;
 
 int main(int argc, char *argv[])
 {
     struct sockaddr_in sockaddr;
     socklen_t server_len = sizeof(sockaddr);
+    uint8_t *write_buffer = { "test message" };
 
     INFO_PRINT("Going to start tcp-client.");
     int tcp_sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -32,12 +35,54 @@ int main(int argc, char *argv[])
         return -1;
     }
     
-    if (connect(tcp_sock, (struct sockaddr *) &sockaddr, server_len) < 0) {
-        ERROR_PRINT("Cannot connect to server ip %s, errno %i", ip, errno);
+    TLSConnection *tls = TLS_init_client();
+    if (tls == NULL) {
+        ERROR_PRINT("Cannot initialize client");
         close(tcp_sock);
         return -1;
     }
 
+    if (CA_certificate_file(&tls) != 1) {
+        ERROR_PRINT("Cannot set client certificate");
+        close(tcp_sock);
+        TLS_free_connection(&tls);
+        return -1;
+    }
+    if (CA_certificate_priv_file(&tls) != 1) {
+        ERROR_PRINT("Cannot set client private key");
+        close(tcp_sock);
+        TLS_free_connection(&tls);
+        return -1;
+    }
+
+    if (connect(tcp_sock, (struct sockaddr *) &sockaddr, server_len) < 0) {
+        ERROR_PRINT("Cannot connect to server ip %s, errno %i", ip, errno);
+        TLS_free_connection(&tls);
+        close(tcp_sock);
+        return -1;
+    }
+
+    if (!TLS_init_ssl_for_socket(&tls, tcp_sock)) {
+        ERROR_PRINT("Could not initialize tls for socket.");
+        close(tcp_sock);
+        TLS_free_connection(&tls);
+        return -1;
+    }
+
+    int ret = SSL_connect(tls->ssl);
+    if (ret != 1) {
+        int err = SSL_get_error(tls->ssl, ret);
+        ERROR_PRINT("SSL Connect was not successfull, error %i", err);
+        ERROR_PRINT("%s", stderr);
+        ERR_print_errors_fp(stderr);  // <-- tämä näyttää oikean virheen
+        close(tcp_sock);
+        TLS_free_connection(&tls);
+        return -1;
+    }
+
+    INFO_PRINT("Going to write %s", write_buffer);
+    SSL_write(tls->ssl, write_buffer, strlen(write_buffer));                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+    TLS_free_connection(&tls);
     close(tcp_sock);
     return 0;
 }
